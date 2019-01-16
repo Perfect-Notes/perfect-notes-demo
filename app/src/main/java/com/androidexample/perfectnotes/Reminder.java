792bc5aa772904d503b5da9270035bc8443cc6de
vibhusehra <vibhusehra@gmail.com>
2019-01-16 18:20:07 +0530

    commit before making changes for the room database

--- app/src/main/java/com/androidexample/perfectnotes/Reminder.java
+++ app/src/main/java/com/androidexample/perfectnotes/Reminder.java
@@ -16,8 +16,40 @@ public class Reminder {
     @ColumnInfo(name = "date")
     public String date;
 
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
     @ColumnInfo(name = "time")
     public String time;
+
+
+
+    public Reminder(String dateList,String timeList, String label){
+        this.date = dateList;
+        this.time = timeList;
+        this.label = label;
+    }
 
 
 }
